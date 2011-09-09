c062cecf62b88ad05ba3a26c9d0f20edfa03469d
Takaaki Muta <takaaki.muta@optim.co.jp>
2011-09-09 20:23:31 +0900

    chatをrails3のプロジェクトで作り直した

---
+++ websocket_chat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsocketChat::Application.config.secret_token = '0f4176943a723d99a895b763c0cf46ef4f6f1b47c85da0f51c4e7d2fb39f08bce63b7308843fbe2a862eed96d63da3d1d019105822c4fd964268da57229a8e6e'
