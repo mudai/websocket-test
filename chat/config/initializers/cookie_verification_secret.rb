5376235ba6170d98effd22cc77c9fde840238def
Takaaki Muta <takaaki.muta@optim.co.jp>
2011-09-09 19:04:22 +0900

    イランモンを削除

--- chat/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '2c0dfa9a254fde6c5f630cb7bf6ee05da9170c9648e5b182430f69df11d05eb2cb044623669418ad0ac2ab55b7085cbdb8837b18b2080358b2547059b9cea842';
